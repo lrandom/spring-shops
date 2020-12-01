b85e32118f0ed3f7e679fd995bb86a022d5df6ad
root <luann4099@gmail.com>
2020-12-01 20:16:06 +0700

    init

---
+++ src/main/java/com/example/shops/ShopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.shops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopsApplication.class, args);
+    }
+
+}
